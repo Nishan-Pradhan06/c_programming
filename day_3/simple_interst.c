84e6bc9951e49a6cb858ad7832a11abc8e1454fd
nishan <nishanpradhan500@gmail.com>
2023-07-27 20:00:58 +0545

    day_2

--- day_3/simple_interst.c
+++ day_3/simple_interst.c
@@ -4,13 +4,13 @@
 int main()
 {
     float principal, rate, time, simple_interest; // creating varibale
-    printf("Enter the Principal amount: "); // taking input from user for principal amount
+    printf("Enter the Principal amount: ");       // taking input from user for principal amount
     scanf("%f", &principal);
     printf("Enter the Rate amount: "); // taking input from user for rate amount
     scanf("%f", &rate);
     printf("Enter the time: "); // taking input from user for time
     scanf("%f", &time);
     simple_interest = (float)(principal * rate * time) / 100;   // applying formula to calculate the si
-    printf("\nThe Simple Interest is %.2f\n", simple_interest); // display ,,%2.f disply more value
+    printf("\nThe Simple Interest is %.2f\n", simple_interest); // display ,,%2.f disply as 2.002
     return 0;
 }

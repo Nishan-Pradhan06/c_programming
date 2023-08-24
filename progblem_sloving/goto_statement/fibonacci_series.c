08c88378547a4bf1f21f34c5dc23a165540b1172
nishan <nishanpradhan500@gmail.com>
2023-08-24 21:14:48 +0545

    problem-sloving

--- progblem_sloving/goto_statement/fibonacci_series.c
+++ progblem_sloving/goto_statement/fibonacci_series.c
@@ -1,15 +1,15 @@
-//wap to disply fibonacci series 0, 1, 1, 2, 3, 5, 8...10th term.. using 
-//go to statement
+// wap to disply fibonacci series 0, 1, 1, 2, 3, 5, 8...10th term.. using
+// go to statement
 #include <stdio.h>
 #include <conio.h>
 int main()
 {
-    int firstnum = 0, secondnum = 1, count = 2, term;
-    printf("10th Fibonacci series are: %d ,%d ", firstnum, secondnum);
+    int firstnum = 0, secondnum = 1, count = 0, term;
+    printf("10th Fibonacci series are: ");
 
 up:
     term = firstnum + secondnum;
-    printf(", %d ", term);
+    printf("%d ", firstnum);
     firstnum = secondnum;
     secondnum = term;
 
